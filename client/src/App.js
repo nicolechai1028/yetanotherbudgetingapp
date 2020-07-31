d2c146f8827f152cf65f04547fbbf2e78e9fde05
Nicole Chai <nicole@namawell.com>
2020-07-30 18:35:45 -0700

    added login and registration pages, also some css styling for login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dummy from './components/Dummy/Dummy.js'
+
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+import { FacebookLoginButton } from 'react-social-login-buttons';
+
+import Login from "./src/pages/Login/"
+
 function App() {
   return (
-    <Dummy/>
+    <Login/>
+
   );
 }
 
