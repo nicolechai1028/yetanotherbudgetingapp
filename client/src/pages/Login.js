2a8cb6eb3b7d63cd14bb3123c26c4c23ffac48d9
Nicole Chai <nicole@namawell.com>
2020-07-31 21:00:47 -0700

    deleted social button

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
+import './App.css';
 
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import { FacebookLoginButton } from 'react-social-login-buttons';
 
 function Login() {
   return (
-    <Form className="login-form">
+    <Form className="login-form" onSubmit={login}>
       <h1><span className="font-weight-bold">
        Yet Another Budgeting App</span></h1>
        <h2 className="text-center">Welcome</h2>
@@ -19,10 +18,6 @@ function Login() {
          <Input type="password" placeholder="Password"></Input>
        </FormGroup>
        <Button className="btn-lg btn-dark btn-block">Log In</Button>
-       <div className="text-center pt-3">
-         Or continue with your social account
-      </div>
-      <FacebookLoginButton className="mt-3 mb-3"/>
       <div className="text-center">
         <a href="/sign-up">Sign-Up</a>
         <span className="p-2"></span>
