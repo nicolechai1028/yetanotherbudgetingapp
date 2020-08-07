1e848c7642503c63cca32f500783dbc042b1e47f
Leo Le <lnle125@gmail.com>
2020-08-07 16:07:19 -0700

    re-integrated more routes

--- client/src/components/MainRoutes.js
+++ client/src/components/MainRoutes.js
@@ -6,15 +6,21 @@ import Overview from "../pages/Overview";
 import Budget from "../pages/Budget/";
 import Login from "../pages/Login/";
 import NavigationBar from "../components/NavigationBar/";
+import Expenses from "../pages/Expense/";
+import Register from "../pages/Register/";
+import Home from "../pages/Home/";
 
 function MainRoutes(props) {
   const [state] = useAppContext();
   return (
     <Switch>
       <Route path="*" component={NavigationBar} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
       {state.user ? (
         <div>
+          <Route exact path="/expense" component={Expenses} />
           <Route exact path="/overview" component={Overview} />
           <Route exact path="/budget" component={Budget} />
         </div>
