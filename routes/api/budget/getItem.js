698edc6b0054ca355d0583e76591e90f883ea557
Chikeobi <chikeobi.njaka@gmail.com>
2020-08-11 18:12:47 -0700

    Completed /api/budget/setItem

--- routes/api/budget/getItem.js
+++ routes/api/budget/getItem.js
@@ -2,13 +2,10 @@
  *                                    HISTORY                                           *
  ****************************************************************************************
  *                                                                                      *
- * == chikeobi-03 ==                                                                    *
- *   +    Created                                                                       *
- *                                                                                      *
- * == chikeobi-05 ==                                                                    *
- *   +    Changed name of file from "getBudget" to "getBudgetItem". Will be used to     *
- *          retreive a single budget item                                               *
- *                                                                                      *
+ * == chikeobi-12 ==                                                                    *
+ *   +  Created                                                                         *
+ *   +                                                                                  *
+ *   +                                                                                  *
  *                                                                                      *
  *                                                                                      *
  *                                                                                      *
@@ -55,17 +52,40 @@ router.route("/").post((req, res) => {
   console.log(Utilities.getFullUrl(req));
   console.log(req.body);
 
-  let { sessionUUID } = req.body;
+  let response,
+  dbProfile,
+  ownerRef,
+  dbResults,
+  dbCategory,
+  dbSubCategory,
+  dbSubCategories,
+  budgetCategory,
+  subgetSubCategories;
 
-  let response = { status: "ERROR", message: "Not yet implemented... sorry..." };
-  (async () => {
+  let { sessionUUID,yearMonth } = req.body;
+  if (!sessionUUID || (sessionUUID = sessionUUID.trim()).length == 0)
+    response = { status: "ERROR", message: "Missing or invalid sessionUUID" };
+  else if (
+    !yearMonth ||
+    (yearMonth = yearMonth.trim()).length == 0 ||
+    isNaN(yearMonth) == true ||
+    yearMonth < Constants.MIN_YYYYMM ||
+    yearMonth > Constants.MAX_YYYYMM
+  )
+    yearMonth = Utilities.getYearMonth();
+
+    if (response) {
+      console.log("Create Budget getItem API Response:\n", response);
+      res.json(response);
+      return;
+    }
+
+    (async () => {
     try {
       dbResults = await db.UserProfile.find({ sessionUUID }).lean(); // use "lean" because we just want "_id"; no virtuals, etc
-      if (!dbResults || dbResults.length == 0) response = { status: "ERROR", message: "Invalid sessionUUID" };
-      else {
-        dbProfile = dbResults[0];
-        ownerRef = dbProfile._id;
-      }
+      if (!dbResults || dbResults.length == 0) throw "Invalid sessionUUID";
+      dbProfile = dbResults[0];
+      ownerRef = dbProfile._id;
     } catch (error) {
       response = { status: "ERROR", message: error.message };
     }
